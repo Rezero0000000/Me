1903b5b6895f3b4589d1331b8c0ac36b5265b872
Rekun <hikarisakata91@gmail.com>
2023-04-03 20:04:38 +0700

    Update Projects page UI

--- src/components/page/Projcets.jsx
+++ src/components/page/Projcets.jsx
@@ -3,12 +3,12 @@ import Navbar from "../molecules/Navbar";
 
 const Projects = () => {
     let Projects = [
-        {id:0,title:"LaraBlog",description:"Sebuah blog system berbasis aplikasi web",repo:"",techs:["php", "laravel", "mariadb", "tailwind"]},
-        {id:1,title:"Ingfobase",description:"Mvc web application build with php native",repo:"",techs:["php", "bootstrap", "mariadb"]},
+        {id:0,title:"LaraBlog",description:"Sebuah blog system berbasis aplikasi web",repo:"",techs:["mysql", "php", "laravel", "tailwind"]},
+        {id:1,title:"Ingfobase",description:"Mvc web application build with php native",repo:"",techs:[ "mysql", "bootstrap","php" ]},
         {id:2,title:"Recrypt",description:"A cli apps to cryptography with classical method",repo:"",techs:["python"]},
         {id:3,title:"Trabot",description:"Landing page Travel application, with Beautiful UI",repo:"",techs:[ "javascript", "react", "tailwind", "vite"]},
-        {id:4,title:"NodeBlog",description:"NodeBlog Sebuah blog system dengan arsitektur monolith",repo:"",techs:["javascript","tailwind","express", "mariadb"]},
-        {id:5,title:"NoteApi",description:"Sebuah Rest Api untuk mengelola catatan",repo:"",techs:["javascript", "express", "mariadb"]},
+        {id:4,title:"NodeBlog",description:"NodeBlog Sebuah blog system dengan arsitektur monolith",repo:"",techs:["javascript","tailwind","expressLight", "mysql"]},
+        {id:5,title:"NoteApi",description:"Sebuah Rest Api untuk mengelola catatan",repo:"",techs:["javascript", "mysql","expressLight"]},
         {id:6,title:"Epicgames-Clone",description:"Sebuah web clone dibuat menggunakan React.js",repo:"",techs:["javascript", "react", "tailwind", "vite"]},
         {id:7,title:"MySite",description:"Personal Website for portofolio",repo:"",techs:["javascript", "react", "tailwind", "vite"]},
     ];
@@ -25,11 +25,20 @@ const Projects = () => {
                     {
                     Projects.map(project => (
                         <div className="bg-[#1A1B1F] p-5 rounded-lg mt-10 border border-[#323439] transition-all duration-300 hover:bg-[#323439] ease-in hover:-translate-y-2 " key={project.id}>
-                            <h1 className="font-semibold text-xl">{project.title}</h1>
-                            <p className="mt-5 mb-3">{project.description}</p>
-                            <ul className="flex gap-2">
-                                {project.techs.map (tech => <li><img className="w-5 h-5" src={`icons/${tech}.svg`} alt="" /></li>)}
-                            </ul>
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <h1 className="font-semibold text-xl">{project.title}</h1>
+                                    <div className="flex items-center">
+                                        <img src="icons/forkLight.svg" className="w-5 h-5" alt="" />
+                                        <img src="icons/linkLight.svg" className="w-6 h-6 ml-3" alt="" />
+                                    </div>
+                                </div>
+                                <p className="mt-5 mb-3">{project.description}</p>
+                                <ul className="flex gap-2">
+                                    {project.techs.map (tech => <li><img className="w-5 h-5" src={`icons/${tech}.svg`} alt="" /></li>)}
+                                </ul>
+                            </div>
+                            
                         </div>
                         
                     ))
