0bcab0ad934af1b32571c93620722c7358a6c435
Muhammad Raihan Alifiandra <97264840+Rekun-dev@users.noreply.github.com>
2023-04-04 17:36:41 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
    theme: {
      extend: {
         fontFamily: {
-          mono: ['"Roboto Mono"', ...defaultTheme.fontFamily.sans]
+          RobotoMono: ['"Roboto Mono"', ...defaultTheme.fontFamily.sans]
         }
      },
      screens : {
