7930c7bcb56537051f17bc855eb2d3ce731da779
Rekun <hikarisakata91@gmail.com>
2023-04-05 07:16:00 +0700

    Add send email feature

--- src/components/molecules/AboutMe.jsx
+++ src/components/molecules/AboutMe.jsx
@@ -3,7 +3,7 @@ const AboutMe = () => {
     return (
         <>
             <div className="md:text-[14px] text-[12.5px]">
-                <strong><h1 className="text-3xl md:text-4xl">About Me</h1></strong>
+                <strong><h1 className="text-2xl md:text-3xl">About Me</h1></strong>
                 <div className="mt-3 mb-7 border-b-2 border-dashed border-slate-400"></div>
                 <p>Hi everyone, my name is <strong> Muhammad Raihan Alifiandra</strong> But you can call me <strong>Rei</strong>. Im a Junior web devloper, Computer Science Student, Free and Open Source Software Ethuesiast. I live in Balikpapan, Indonesia and I'm 19 year old. </p>
                 <p className="mt-3">Coding has been my passion and hobby since I was 15 years old, and I love computer science I was a kid. Besides coding. I love listening to music and drawing. Sometimes I watch anime, but I prefer read manga and novel, I spend a lot of time on Facebook sharing memes and shitposts</p>
--- src/components/page/Contact.jsx
+++ src/components/page/Contact.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../molecules/Navbar";
 import Footer from "../molecules/Footer";
 
 const Contact = () => {
+    const [formState, setFormState] = useState({});
+
+    const changeHandler = (event) => {
+        setFormState({...formState, [event.target.name]: event.target.value});
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const config = {
+            Username: "muhammadraihanalifiandra5@gmail.com",
+            Password: "B526C7FD2A6049146730C160462AB3747BC6",
+            Host:"smtp.elasticemail.com",
+            Port: 2525,
+            SecureToken: "fa2a63ca-8ab1-48b9-818d-0a4cc137cd47",
+            To : 'muhammadraihanalifiandra5@gmail.com',
+            From : formState.email,
+            Subject : formState.subject,
+            Body : `${formState.name}, ${formState.message}`,
+        };
+
+        if(window.Email) {
+            window.Email.send(config).then(() => {
+                alert("Email sent successfully")
+            });
+        }
+    }
+
     let contacts = [
         {id: 0, name:"Facebook", url:"github.com", icons:"facebookLight"},
         {id: 1, name:"Instagram", url:"github.com", icons:"instagramLight"},
@@ -15,17 +42,17 @@ const Contact = () => {
         <>
             <Navbar />
             <section className="md:px-32 lg:px-40 px-5 py-20 md:py-24 lg:py-28 xl:px-60 ">
-                <strong><h1 className="text-3xl md:text-4xl mt-7 mb-3">Contact</h1></strong>
+                <strong><h1 className="text-2xl md:text-3xl mt-7 mb-3">Contact</h1></strong>
                 <div className="mt-3  border-b-2 border-dashed border-slate-400"></div>
-                <form action="" className="mt-10 ">
-                    <input type="text" placeholder="Your Name" className="rounded-lg border-2 md:text-[14px] text-[12px] w-full shadow-[#42434a] dark:bg-[#F3F4F6] bg-[#1A1B1F]"/>
-                    <input type="text" placeholder="Your Email" className="rounded-lg border-2 md:text-[14px] text-[12px]  w-full shadow-[#42434a] dark:bg-[#F3F4F6] bg-[#1A1B1F] my-3" />
-                    <input type="text" placeholder="Subject" autoComplete="off" className="rounded-lg border-2 md:text-[14px] text-[12px]  w-full shadow-[#42434a] dark:bg-[#F3F4F6] bg-[#1A1B1F] mb-3" />
-                    <textarea rows="6" placeholder="Your Message" className="border-2 md:text-[14px] text-[12px] py-2 px-2.5 dark:bg-[#F3F4F6] dark:shadow-none  w-full bg-[#1A1B1F]  rounded-lg shadow-md shadow-[#42434a]" name="message" id="message"></textarea>
+                <form action="" className="mt-10" onSubmit={submitHandler}>
+                    <input value={formState.name || ''} onChange={changeHandler} name="name" type="text" placeholder="Your Name" className="rounded-lg border-2 md:text-[14px] text-[12px] w-full shadow-[#42434a] dark:bg-[#F3F4F6] bg-[#1A1B1F]"/>
+                    <input value={formState.email || ''} onChange={changeHandler} name="email"  type="email" placeholder="Your Email" className="rounded-lg border-2 md:text-[14px] text-[12px]  w-full shadow-[#42434a] dark:bg-[#F3F4F6] bg-[#1A1B1F] my-3" />
+                    <input value={formState.subject || ''} onChange={changeHandler} name="subject"  type="text" placeholder="Subject" autoComplete="off" className="rounded-lg border-2 md:text-[14px] text-[12px]  w-full shadow-[#42434a] dark:bg-[#F3F4F6] bg-[#1A1B1F] mb-3" />
+                    <textarea value={formState.message || ''} onChange={changeHandler} rows="6" placeholder="Your Message" className="border-2 md:text-[14px] text-[12px] py-2 px-2.5 dark:bg-[#F3F4F6] dark:shadow-none  w-full bg-[#1A1B1F]  rounded-lg shadow-md shadow-[#42434a]" name="message" id="message"></textarea>
                     <button className="mt-5 dark:text-white text-[14px] bg-[#42434a] hover:dark:text-[#364252] hover:dark:bg-[#F3F4F6] dark:hover:border hover:dark:border-[#42434a] hover:text-white px-6  py-2 rounded-lg ">Send</button>
                 </form>
                 <div className="mt-7">
-                    <strong><h1 className="text-3xl md:text-4xl mb-3">Follow Me</h1></strong>
+                    <strong><h1 className="text-2xl md:text-3xl mb-3">Follow Me</h1></strong>
                     <div className="mt-3  border-b-2 border-dashed border-slate-400"></div>
                     <ul className="flex gap-3 mt-2">
                     {
