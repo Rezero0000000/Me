70000c2d2936897e50d55e966dc47837ba4e1f9f
Muhammad Raihan Alifiandra <97264840+Rekun-dev@users.noreply.github.com>
2023-04-05 12:45:06 +0800

    Update AboutMe.jsx

--- src/components/molecules/AboutMe.jsx
+++ src/components/molecules/AboutMe.jsx
@@ -5,17 +5,17 @@ const AboutMe = () => {
             <article className="md:text-[14px] text-[12.5px]">
                 <strong><h1 className="text-2xl md:text-3xl">About Me</h1></strong>
                 <div className="mt-3 mb-7 border-b-2 border-dashed border-slate-400"></div>
-                <p>Hi everyone, my name is <strong> Muhammad Raihan Alifiandra</strong> But you can call me <strong>Re</strong>. Im a Junior web devloper, Computer Science Student, Free and Open Source Software Ethuesiast. I live in Balikpapan, Indonesia and I'm 19 year old. </p>
-                <p className="mt-3">Coding has been my passion and hobby since I was 15 years old, and I love computer science. Besides coding. I love listening to music and drawing. Sometimes I watch anime, but I prefer read manga and novel, I spend a lot of time on Facebook sharing memes and shitposts</p>
+                <p>Hi everyone, my name is <strong> Muhammad Raihan Alifiandra</strong> But you can call me <strong>Re</strong>. I'm a Junior web Developer, Computer Science Student, Free and Open Source Software Ethuesiast. I live in Balikpapan, Indonesia and I'm 19 year old. </p>
+                <p className="mt-3">Coding has been my passion and hobby since I was 15 years old, and I love computer science. Besides coding. I love listening to music and drawing. Sometimes I watch anime, but I prefer to read manga and novel, I spend a lot of time on Facebook sharing memes and shitposts</p>
             </article>
             <article className="mt-10">
                <strong><h1 className="text-2xl lg:text-3xl mb-5">What I'm Doing Now</h1></strong> 
                 <div className="mt-3 mb-7 border-b-2 border-dashed border-slate-400"></div>
                 <ul className="list-disc ml-5 md:text-[14px] text-[12.5px]">
                     <li>Enjoying Life. exactly</li>
-                    <li>Go to College</li>
-                    <li>Learn to be more productive</li>
-                    <li>Learn Golang, Cryptography, Pentesting</li>
+                    <li>Going to College</li>
+                    <li>Learning to be more productive</li>
+                    <li>Learning Golang, Cryptography, Pentesting</li>
                 </ul>
             </article>
         </>
