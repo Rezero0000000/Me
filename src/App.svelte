a45538834a1a03491600c0fe3bed2bf247831287
Rezero <hikarisakata91@gmail.com>
2024-05-06 08:48:15 +0700

    Setup

---
+++ src/App.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { Router, Link, Route } from "svelte-routing";
+  import About from "./routes/About.svelte";
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  const url = ""
+</script>
+<Router {url}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/blog">Blog</Link>
+  </nav>
+  <div>
+    <Route path="/about" component={About} />
+  </div>
+</Router>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/routes/About.svelte
@@ -0,0 +1,25 @@
+
+<main>
+    <div>
+        <h1>HAHAHA HOME</h1>
+      <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <img class="logo" alt="Vite Logo" />
+      </a>
+      <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+        <img  class="logo svelte" alt="Svelte Logo" />
+      </a>
+    </div>
+    <h1>Vite + Svelte</h1>
+  
+    <div class="card">
+    </div>
+  
+    <p>
+      Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    </p>
+  
+    <p class="read-the-docs">
+      Click on the Vite and Svelte logos to learn more
+    </p>
+  </main>
+  
